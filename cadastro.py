96335cef24ecccee54dcbfc6a7ffd60011c5f5e6
Desktop <danielzsanxhes@gmail.com>
2024-08-20 11:08:31 -0400

    as

--- cadastro.py
+++ cadastro.py
@@ -1,12 +1,15 @@
 import tkinter as ttk
 
-var=   ttk.Tk()
+var= ttk.Tk()
 var.geometry("600x600")
-frame= ttk.Frame(var, pady=5,padx=5,background="red")
+frame= ttk.Frame(var, pady=12,padx=12,background="#008b8b")
 
 var.title("Supermercati a Genova")
+h1=ttk.Label(frame, text="Salve mio signiore, ciao mia signora, Benvenuto al registro site web ", background="#008b8b", foreground="white")
+h1.grid(column=4,row=0)
 
-h1=ttk.Label(frame, text="Salve mio signiore, ciao mia signora, Benvenuto al registro site web ")
-h1.grid(sticky="w",column=1,row=0)
+h2=ttk.Label(frame)
+
+frame.grid()
 
 var.mainloop()
