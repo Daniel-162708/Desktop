d2a9f79939ca5262f7ca6b69561d38388cb34077
Desktop <danielzsanxhes@gmail.com>
2024-08-22 11:06:11 -0400

    dd

--- Exercicio_2.py
+++ Exercicio_2.py
@@ -1,10 +1,11 @@
 import tkinter as ttk
-var= ttk.Tk() 
-var_para_o_frame= ttk.Frame(var,pady= 300,padx=300, background=("#41D1CC"))
-var_para_o_frame.grid()
+
+var= ttk.Tk() #
+var_para_o_frame= ttk.Frame(var,pady= 300,padx=300, background=("#41D1CC")) #criei o frame da janela
+var_para_o_frame.grid() #
 contador=0
 
-def soma():
+def soma(): #função de soma
     global contador 
     contador+=1
     valores.configure(text=f"{contador}",foreground="Red")
--- cadastro.py
+++ cadastro.py
@@ -2,14 +2,16 @@ import tkinter as ttk
 
 var= ttk.Tk()
 var.geometry("600x600")
-frame= ttk.Frame(var, pady=12,padx=12,background="#008b8b")
+frame= ttk.Frame(var, pady=12,padx=12,background=("#00bfff"))
+frame.pack()
+var.configure(background="#4169e1")
 
 var.title("Supermercati a Genova")
-h1=ttk.Label(frame, text="Salve mio signiore, ciao mia signora, Benvenuto al registro site web ", background="#008b8b", foreground="white")
-h1.grid(column=4,row=0)
+h1=ttk.Label(frame, text="Salve mio signiore, ciao mia signora, Benvenuto al registro site web ",bg="#00bfff", foreground="white")
+h1.pack()
+h1.grid(column=0,row=0)
 
-h2=ttk.Label(frame)
-
-frame.grid()
+h2=ttk.Entry(var,bg="white")
+h2.pack(pady="200")
 
 var.mainloop()
