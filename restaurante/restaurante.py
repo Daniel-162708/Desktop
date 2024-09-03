92d37b0aaaf5aaa578ccabdbcd3d645f33ca3e68
Daniel_162708 <danielzsanxhes@gmail.com>
2024-09-03 11:08:46 -0400

    aa

--- restaurante/restaurante.py
+++ restaurante/restaurante.py
@@ -6,9 +6,10 @@ import tkinter as tk
 class App(ctk.CTk):
     def __init__(self):
         super().__init__()
-        
+        self.frame1=ctk.CTkFrame(self, width=500,height=550, bg_color="White")
+        self.frame1.place(x=40,y=200)
 
 app = App()
-app.geometry("600x600") 
+app.geometry("1920x1080") 
 app.title("Restaurante")
 app.mainloop()
